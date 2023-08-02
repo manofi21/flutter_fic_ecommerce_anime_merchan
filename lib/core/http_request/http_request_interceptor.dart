1e95596fd054a2ba89dda75e509574fb1debe8ee
unknown <jeremimanogi@gmail.com>
2023-08-02 13:50:54 +0700

    adding checking push to authpage if token invalid

--- lib/core/http_request/http_request_interceptor.dart
+++ lib/core/http_request/http_request_interceptor.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_fic_ecommerce_warung_comicon/core/http_request/is_token_expired.dart';
+import 'package:flutter_fic_ecommerce_warung_comicon/core/show_dialog/show_error_dialog.dart';
+import 'package:flutter_fic_ecommerce_warung_comicon/feature/authentication/presentation/page/auth_page.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 import '../../feature/authentication/data/data_source/auth_local_data_source.dart';
+import '../navigator/navigator.dart';
 import 'status_code_handler.dart';
 
 class HttpRequestInterceptor implements InterceptorContract {
@@ -14,35 +19,52 @@ class HttpRequestInterceptor implements InterceptorContract {
   Future<RequestData> interceptRequest({required RequestData data}) async {
     if (data.method == Method.GET) {
       final getToken = await authStorage.getAccessToken();
-      data.headers[HttpHeaders.authorizationHeader] = "Bearer $getToken"; 
+      data.headers[HttpHeaders.authorizationHeader] = "Bearer $getToken";
     }
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
-      final response = data.body != null ? jsonDecode(data.body!) : {};
-      final errorMessage = response['error'];
-      if (errorMessage != null) {
-        var resultError = errorMessage['message'];
+    final response = data.body != null ? jsonDecode(data.body!) : {};
+    final errorMessage = response['error'];
 
-        /// If "details" == null set "errors" Empty
-        errorMessage["details"] ??= {"errors" : []};
-        final getDetailErros = errorMessage["details"]["errors"];
-        if (getDetailErros is List && getDetailErros.isNotEmpty) {
-          for (Map msg in getDetailErros) {
-            resultError += '\n - ${msg["message"]}';
-          }
+    final isExpired = isTokenExpired(data.statusCode);
+    if (errorMessage != null && !isExpired) {
+      var resultError = errorMessage['message'];
+
+      /// If "details" == null set "errors" Empty
+      errorMessage["details"] ??= {"errors": []};
+      final getDetailErros = errorMessage["details"]["errors"];
+      if (getDetailErros is List && getDetailErros.isNotEmpty) {
+        for (Map msg in getDetailErros) {
+          resultError += '\n - ${msg["message"]}';
         }
-
-        throw resultError;
       }
 
-      final exception = statusCodeHandler(data.statusCode);
-      if (exception != null) {
-        throw exception;
-      }
-    
+      throw resultError;
+    }
+
+    final exception = statusCodeHandler(data.statusCode);
+
+    if (isExpired && exception != null) {
+      final context = navigatorKey.currentContext!;
+      await showErrorDialog(context: context, message: exception.message).then(
+        (value) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => const AuthPage(),
+            ),
+          );
+        },
+      );
+    }
+
+    if (exception != null) {
+      throw exception;
+    }
+
     return data;
   }
 }
