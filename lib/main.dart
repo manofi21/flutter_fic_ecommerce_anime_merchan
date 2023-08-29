9cae65bd8cf163e2fb5210da8240a57c3cc57d82
unknown <jeremimanogi@gmail.com>
2023-08-29 21:25:36 +0700

    adding localization

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_fic_ecommerce_warung_comicon/feature/authentication/presentation/widget/auth_loading_overlay.dart';
 // import 'package:flutter_fic_ecommerce_warung_comicon/core/result_handler/no_params.dart';
 import 'package:flutter_fic_ecommerce_warung_comicon/locator.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sizer/sizer.dart';
 
 import 'core/navigator/navigator.dart';
@@ -32,6 +33,10 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
         home: Sizer(builder: (context, orientation, deviceType) {
           // return const MyHomePage(title: 'Flutter Demo Home Page');
           return const AuthLoadingOverlay(
