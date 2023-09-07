71eaccd806a6b9812198deb65e453385b11553f6
unknown <jeremimanogi@gmail.com>
2023-09-07 18:29:25 +0700

    adding choosed address title widget

---
+++ lib/feature/address/presentation/widget/choosed_address_title.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/choosed_address/choosed_address_cubit.dart';
+import '../cubit/choosed_address/choosed_address_state.dart';
+
+class ChoosedAddressTitle extends StatelessWidget {
+  const ChoosedAddressTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ChoosedAddressCubit, ChoosedAddressState>(
+      builder: (context, state) {
+        if (state.isLoading) {
+          return const Text('Mengambil data');
+        }
+
+        if (state.isSuccess) {
+          return InkWell(
+            onTap: () {},
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "Alamat Penerima : ${state.successValue?.address}",
+                  ),
+                ),
+                const SizedBox(width: 3),
+                const Icon(Icons.keyboard_arrow_down_outlined)
+              ],
+            ),
+          );
+        }
+
+        if (state.isError) {
+          return Text(state.msgError);
+        }
+
+        return Container();
+      },
+    );
+  }
+}
--- lib/feature/dashboard/presentation/widget/search_bar_dashboard_widget.dart
+++ lib/feature/dashboard/presentation/widget/search_bar_dashboard_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../address/presentation/cubit/choosed_address/choosed_address_cubit.dart';
+import '../../../address/presentation/cubit/choosed_address/choosed_address_state.dart';
+import '../../../address/presentation/widget/choosed_address_title.dart';
 import '../../../cart/presentation/widget/cart_badges.dart';
 import '../../../search/presentation/page/search_page.dart';
 
@@ -15,70 +19,84 @@ class _SearchBarDashboardWidgetState extends State<SearchBarDashboardWidget> {
   final tagHero = "hero_from_dashboard";
 
   @override
+  void initState() {
+    super.initState();
+    context.read<ChoosedAddressCubit>().onGetChoosedAddress();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.lightGreen,
       padding: const EdgeInsets.all(15),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => SearchPageBloc(tagHero: tagHero),
-                  ),
-                );
-              },
-              child: Hero(
-                tag: tagHero,
-                child: Material(
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.white54, strokeAlign: 1),
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Icon(Icons.search, size: 17),
-                        Padding(
-                          padding: EdgeInsets.only(left: 2.0),
-                          child: Text(
-                            "Cari di Tokopedia",
-                            style: TextStyle(fontSize: 17.5),
-                          ),
-                        )
-                      ],
+          Row(
+            children: [
+              Expanded(
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => SearchPageBloc(tagHero: tagHero),
+                      ),
+                    );
+                  },
+                  child: Hero(
+                    tag: tagHero,
+                    child: Material(
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          border:
+                              Border.all(color: Colors.white54, strokeAlign: 1),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Icon(Icons.search, size: 17),
+                            Padding(
+                              padding: EdgeInsets.only(left: 2.0),
+                              child: Text(
+                                "Cari di Tokopedia",
+                                style: TextStyle(fontSize: 17.5),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                child: Icon(Icons.mail, size: 23),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                child: Icon(Icons.notifications, size: 23),
+              ),
+              const CartBadges(
+                badgesEnd: 10,
+                child: SizedBox(
+                  width: 23,
+                  child: Icon(Icons.shopping_cart_rounded),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                child: Icon(Icons.menu, size: 23),
+              )
+            ],
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: Icon(Icons.mail, size: 23),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: Icon(Icons.notifications, size: 23),
-          ),
-          const CartBadges(
-            badgesEnd: 10,
-            child: SizedBox(
-              width: 23,
-              child: Icon(Icons.shopping_cart_rounded),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: Icon(Icons.menu, size: 23),
-          )
+          const SizedBox(height: 10),
+          const ChoosedAddressTitle()
         ],
       ),
     );
