3778332071ac3d5d00060c005434cc33229cd988
unknown <jeremimanogi@gmail.com>
2023-09-07 18:17:59 +0700

    create use cases GetChoosedAddressUser

---
+++ lib/feature/address/domain/use_cases/get_choosed_address_user.dart
@@ -0,0 +1,24 @@
+import '../../../../core/errors/failure.dart';
+import '../../../../core/result_handler/no_params.dart';
+import '../../../../core/result_handler/result.dart';
+import '../../../../core/use_cases/future_result_use_case.dart';
+import '../entities/choosed_address_entities.dart';
+import '../repos/address_repo.dart';
+
+class GetChoosedAddressUser extends FutureResultUseCase<ChoosedAddressEntities?, NoParams> {
+  final AddressRepo productRepo;
+  GetChoosedAddressUser(this.productRepo);
+
+  @override
+  Future<Result<ChoosedAddressEntities?, Failure>> processCall(NoParams params) async {
+    try {
+      final getListAddress = await productRepo.getChoosedAddressUser();
+      return Ok(getListAddress);
+    } on Failure {
+      rethrow;
+    } catch (e, stackTrace) {
+      // Mau di return Err atau langsung throw pun juga bisa
+      throw UnknownFailure('Occure in Get Choosed Address User Use Cases : ${e.toString()}', stackTrace);
+    }
+  }
+} 
