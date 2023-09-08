5184fad460adce7c09b3f90e5810184d2840f604
unknown <jeremimanogi@gmail.com>
2023-09-08 23:45:57 +0700

    create add address in address repo

--- lib/feature/address/data/repos/address_repo_impl.dart
+++ lib/feature/address/data/repos/address_repo_impl.dart
@@ -1,10 +1,11 @@
+import 'package:flutter_fic_ecommerce_warung_comicon/feature/address/domain/entities/add_address_entities.dart';
 import 'package:flutter_fic_ecommerce_warung_comicon/feature/address/domain/entities/address_entities.dart';
 import 'package:flutter_fic_ecommerce_warung_comicon/feature/address/domain/repos/address_repo.dart';
 
 import '../../../../core/errors/exceptions.dart';
 import '../../../../core/errors/failure.dart';
-import '../../domain/entities/choosed_address_entities.dart';
 import '../data_source/address_remote_data_source.dart';
+import '../model/add_address_model.dart';
 
 class AddressRepoImpl extends AddressRepo {
   final AddressRemoteDataSource addressRemoteDataSource;
@@ -48,15 +49,15 @@ class AddressRepoImpl extends AddressRepo {
       if (getAddressFromRemote != null) {
         final e = getAddressFromRemote;
         return AddressEntities(
-              id: e.id,
-              labelAddress: e.labelAddress,
-              fullAddress: e.fullAddress,
-              detailAddress: e.detailAddress,
-              recipientName: e.recipientName,
-              phoneNumber: e.phoneNumber,
-              isMainAddress: e.isMainAddress,
-              isChoosedAddress: e.isChoosedAddress,
-            );
+          id: e.id,
+          labelAddress: e.labelAddress,
+          fullAddress: e.fullAddress,
+          detailAddress: e.detailAddress,
+          recipientName: e.recipientName,
+          phoneNumber: e.phoneNumber,
+          isMainAddress: e.isMainAddress,
+          isChoosedAddress: e.isChoosedAddress,
+        );
       }
       return null;
     } on HttpException catch (e) {
@@ -66,6 +67,32 @@ class AddressRepoImpl extends AddressRepo {
     } catch (e, stackTrace) {
       throw UnknownFailure(
           'Occure in Address Repo getChoosedAddressUser : ${e.toString()}',
+          stackTrace);
+    }
+  }
+
+  @override
+  Future<bool> addAddressUserInput(AddAddressEntities addAddress) async {
+    try {
+      final addressRequestUser = AddAddressModel(
+        data: AddAddressDataModel(
+            labelAddress: addAddress.labelAddress,
+            fullAddress: addAddress.fullAddress,
+            detailAddress: addAddress.detailAddress,
+            recipientName: addAddress.recipientName,
+            phoneNumber: addAddress.phoneNumber,
+            isMainAddress: addAddress.isMainAddress,
+            isChoosedAddress: addAddress.isChoosedAddress),
+      );
+      final getAddressFromRemote = await addressRemoteDataSource.addAddress(addressRequestUser);
+      return getAddressFromRemote != null;
+    } on HttpException catch (e) {
+      throw AddressFailure(e.message);
+    } on UnknownException catch (e) {
+      throw AddressFailure(e.message);
+    } catch (e, stackTrace) {
+      throw UnknownFailure(
+          'Occure in Address Repo addAddressUserInput : ${e.toString()}',
           stackTrace);
     }
   }
--- lib/feature/address/domain/repos/address_repo.dart
+++ lib/feature/address/domain/repos/address_repo.dart
@@ -1,6 +1,8 @@
+import '../entities/add_address_entities.dart';
 import '../entities/address_entities.dart';
 
 abstract class AddressRepo {
   Future<List<AddressEntities>> getListAddressUser();
   Future<AddressEntities?> getChoosedAddressUser();
+  Future<bool> addAddressUserInput(AddAddressEntities addAddress);
 }
