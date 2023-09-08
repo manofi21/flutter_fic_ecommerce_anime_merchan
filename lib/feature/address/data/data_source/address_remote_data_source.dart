0ba457f4eb8b3b697c0f13088040250ff4fb2160
unknown <jeremimanogi@gmail.com>
2023-09-08 23:45:31 +0700

    create add address in address remote data source

--- lib/feature/address/data/data_source/address_remote_data_source.dart
+++ lib/feature/address/data/data_source/address_remote_data_source.dart
@@ -3,10 +3,12 @@ import 'package:http_interceptor/http/intercepted_http.dart';
 
 import '../../../../core/errors/exceptions.dart';
 import '../../../../core/http_request/remote_data_request.dart';
+import '../model/add_address_model.dart';
 
 abstract class AddressRemoteDataSource {
   Future<List<AddressModel>> getListAddress();
   Future<AddressModel?> getChoosedAddress();
+  Future<AddressModel?> addAddress(AddAddressModel addressRequest);
 }
 
 class AddressRemoteDataSourceImpl extends RemoteDataRequest
@@ -20,9 +22,7 @@ class AddressRemoteDataSourceImpl extends RemoteDataRequest
     try {
       final addressResult = await getRequest<List<AddressModel>>(
         '/api/addresshes',
-        queryParameters: {
-          'populate':'*'
-        },
+        queryParameters: {'populate': '*'},
         fromMap: (e) {
           final getData = e['data'];
           if (getData is List) {
@@ -42,7 +42,7 @@ class AddressRemoteDataSourceImpl extends RemoteDataRequest
       );
     }
   }
-  
+
   @override
   Future<AddressModel?> getChoosedAddress() async {
     try {
@@ -63,6 +63,30 @@ class AddressRemoteDataSourceImpl extends RemoteDataRequest
     } catch (e, stackTrace) {
       throw UnknownException(
         'Occure in Address Remote Data Source(getChoosedAddress) : ${e.toString()}',
+        stackTrace,
+      );
+    }
+  }
+
+  @override
+  Future<AddressModel?> addAddress(AddAddressModel addressRequest) async {
+    try {
+      final addressResult = await postRequest<AddressModel>(
+        '/api/addresshes',
+        bodyParameter: addressRequest.toJson(),
+        useEncode: true,
+        fromMap: (e) {
+          final getData = e['data'];
+          final listResut = AddressModel.fromJson(getData);
+          return listResut;
+        },
+      );
+      return addressResult;
+    } on HttpException {
+      rethrow;
+    } catch (e, stackTrace) {
+      throw UnknownException(
+        'Occure in Address Remote Data Source(getListAddress) : ${e.toString()}',
         stackTrace,
       );
     }
