ffb6e44f5a120e6291b9901aa7477513536b9956
unknown <jeremimanogi@gmail.com>
2023-09-12 21:12:15 +0700

    add list product in bottom sheet cart

--- lib/feature/product/presentation/widget/bottom_sheet_to_cart.dart
+++ lib/feature/product/presentation/widget/bottom_sheet_to_cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fic_ecommerce_warung_comicon/feature/cart/presentation/page/cart_page.dart';
+import 'package:flutter_fic_ecommerce_warung_comicon/feature/product/presentation/page/product_page.dart';
 
 import '../../../../core/bottom_sheet/bottom_sheet.dart';
 import '../../../../core/constant/constant.dart';
@@ -11,63 +12,75 @@ void bottomSheetToCart({
 }) {
   showBaseBottomSheet(
     currentcontext: context,
-    builder: Card(
-      elevation: 5,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
+    builder: SizedBox(
+      height: (MediaQuery.of(context).size.height * (2 / 3) - 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card(
+            elevation: 5,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          'http://$baseUrl${productItem.urlImages.first.urlSmallImage}'),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'http://$baseUrl${productItem.urlImages.first.urlSmallImage}'),
+                        ),
+                      ),
                     ),
-                  ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          const Text(
+                            'Product berhasil ditambahkan',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            productItem.productName,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            softWrap: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      const Text(
-                        'Product berhasil ditambahkan',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        productItem.productName,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                        softWrap: true,
-                      ),
-                    ],
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => const CartPage()));
+                  },
+                  child: const SizedBox(
+                    height: 20,
+                    width: double.infinity,
+                    child: Text('Lihat Keranjang', textAlign: TextAlign.center),
                   ),
                 ),
               ],
             ),
-            OutlinedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const CartPage()));
-              },
-              child: const SizedBox(
-                height: 20,
-                width: double.infinity,
-                child: Text('Lihat Keranjang', textAlign: TextAlign.center),
-              ),
-            ),
-          ],
-        ),
+          ),
+          Text("Lihat Product Lainnya",
+              style: Theme.of(context).textTheme.headlineSmall),
+          SizedBox(
+            height: (MediaQuery.of(context).size.height / 2) - 40,
+            child: const ProductPage(),
+          )
+        ],
       ),
     ),
   );
